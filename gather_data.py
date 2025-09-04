a8e8b5c76fa9525b36a2cd1bc847ffeb6e254689
zkornbluth <zkornbluth2007@gmail.com>
2025-09-04 17:16:00 -0400

    A few changes
    - Added 14 more pitchers (all CYA runner ups, 2 winners - Greinke, Hernandez)
    - Filtered unused columns in gather_data.py so they're not saved to .csv file (it's much smaller now)
    - added requirements.txt
    - removed old unpackaged .twb, the .twbx is correct

--- gather_data.py
+++ gather_data.py
@@ -5,22 +5,28 @@ This file was run to gather the data in the .csv file that will be used on the d
 from pybaseball import playerid_lookup, statcast_pitcher
 import pandas as pd
 
-# Starting with list of last 10 different Cy Young winners per league - 18 total (Snell won both, cutting Dickey as he only pitched until 2017)
+# 18 most recent Cy Young winners, followed by 14 most recent runners up (which includes 2 older winners)
 players_list = ["Tarik Skubal", "Chris Sale", "Gerrit Cole", "Blake Snell", "Justin Verlander", "Sandy Alcántara",
                 "Robbie Ray", "Corbin Burnes", "Shane Bieber", "Trevor Bauer", "Jacob deGrom", "Corey Kluber",
-                "Max Scherzer", "Rick Porcello", "Dallas Keuchel", "Jake Arrieta", "Clayton Kershaw", "David Price"]
+                "Max Scherzer", "Rick Porcello", "Dallas Keuchel", "Jake Arrieta", "Clayton Kershaw", "David Price",
+                "Seth Lugo", "Zack Wheeler", "Sonny Gray", "Logan Webb", "Dylan Cease", "Max Fried", "Kenta Maeda",
+                "Yu Darvish", "Hyun Jin Ryu", "Jon Lester", "Zack Greinke", "Félix Hernández", "Johnny Cueto",
+                "Adam Wainwright"]
+
+all_player_stats = []
 
 all_player_stats = []
 
 for player_name in players_list:
-# player_name = players_list[16]
     player_name_lst = player_name.split(" ")
+    if player_name == "Hyun Jin Ryu": # Hyun Jin is the first name - need to manually correct splitting
+        player_name_lst = ["Hyun Jin", "Ryu"]
 
     player_ids = playerid_lookup(player_name_lst[1], player_name_lst[0])
+    
     mlbam_key = player_ids['key_mlbam'][0]
 
-    player_stats = statcast_pitcher('2016-03-01', "2024-11-01", mlbam_key) # Start in 2016 when statcast was everywhere, through end of 2024 playoffs
-
+    player_stats = statcast_pitcher('2016-03-01', '2024-11-01', int(mlbam_key)) # Start in 2016 when statcast was everywhere, through end of 2024 playoffs
     player_stats = player_stats[player_stats['game_type'].isin(['R', 'P', 'F', 'D', 'L', 'W'])] # Filter out S (spring training) and E (exhibition)
 
     player_stats['pitch_type'] = player_stats['pitch_type'].replace('FA', 'FF') # Both codings mean four-seam fastball
@@ -50,13 +56,14 @@ pitch_types = {
     'GY': 'Gyroball',
     'SV': 'Sweeper',
     'ST': 'Slurve',
+    'CS': 'Slow Curve',
     'PO': 'Pitchout',
     'IN': 'Intentional Ball',
     'UN': 'Unknown'
 }
 
-# Filter out pitchout, intentional ball, unknown, blank (irrelevant to a pitcher's arsenal)
-all_player_stats_joined = all_player_stats_joined[~all_player_stats_joined['pitch_type'].isin(["PO", "IN", "UN", ""])]
+# Filter out eephus, pitchout, intentional ball, unknown, blank (irrelevant to a pitcher's arsenal)
+all_player_stats_joined = all_player_stats_joined[~all_player_stats_joined['pitch_type'].isin(["EP", "PO", "IN", "UN", ""])]
 
 # Change pitch_type from coding to name
 all_player_stats_joined['pitch_type'] = all_player_stats_joined['pitch_type'].map(lambda code: pitch_types.get(code, code))
